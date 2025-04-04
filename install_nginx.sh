7587fc4e0e07cd1b2ea9dd942f0b97e7c7e93eaa
dishant dudhat <dobariyadishant@gmail.com>
2025-04-04 14:30:50 +0530

    Added shell script

--- install_nginx.sh
+++ install_nginx.sh
@@ -5,4 +5,4 @@ sudo apt-get install nginx -y
 sudo systemctl start nginx
 sudp systemctl enable nginx
 
-echo "<h1>nginx install while instance is created</h1>"
+echo "<h1>nginx install while instance is created</h1>" > /var/www/html/index.html
