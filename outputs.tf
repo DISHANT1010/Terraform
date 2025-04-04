7ec8fab57f1ed68feabf01146e563c9448dd9771
dishant dudhat <dobariyadishant@gmail.com>
2025-04-04 14:16:59 +0530

    Added varaible file

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "ec2_public_ip" {
+  value = aws_instance.terraform-instance.public_ip
+}
+
+output "ec2_public_dns" {
+  value = aws_instance.terraform-instance.public_dns
+}
+
+output "ec2_private_ip" {
+  value = aws_instance.terraform-instance.private_ip
+}
