4bb3577821aac25c069f251e672503e726e991ef
dishant dudhat <dobariyadishant@gmail.com>
2025-04-05 12:19:42 +0530

    Added backend

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,12 @@ terraform {
         version = "5.91.0"
     }
   }
+
+  backend "s3" {
+    bucket = "remot_backend_s3"
+    key = "terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "lockfile_dynamodb_table"
+
+  }
 }
